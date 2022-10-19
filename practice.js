bc75196a14367de1f952d3054a2515f5edd87ab4
gautamkumar1912 <91456141+gautamkumar1912@users.noreply.github.com>
2022-10-19 11:54:10 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -14,18 +14,4 @@ if(count == 2){
 }
 
 
-//Check if the Number is palindrome 
 
-var num = 1221;
-var n = String(num);
- var bag = "" ;
- for(var i = n.length-1; i >= 0; i--){
-     bag+=i;
-     
- }
- bag = Number(bag);
- if(bag == num){
-     console.log("Yes");
- }else{
-     console.log("No");
- }
