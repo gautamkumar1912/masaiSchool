a2a7f962f34dd7774bd2b84733ff33dbfbe7abf8
gautamkumar1912 <91456141+gautamkumar1912@users.noreply.github.com>
2022-10-19 11:53:23 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,22 +1,31 @@
-
-// My Login Credentials
-var username = "gautamsinghgolu12@gmai.com";
-var password = "123@123";
-
-
-
-
-
-
-
-
-
-// Finding odd numbers in an array
+// check if num is prime or not
 
 var num = 10;
 var count = 0;
 for(var i = 1; i<= num; i++){
-if(i%2==1){
-console.log(i);
+if(i%num==0){
+count++
 }
 }
+if(count == 2){
+  console.log("It is a prime number");
+}else{
+  console.log("Not a prime number");
+}
+
+
+//Check if the Number is palindrome 
+
+var num = 1221;
+var n = String(num);
+ var bag = "" ;
+ for(var i = n.length-1; i >= 0; i--){
+     bag+=i;
+     
+ }
+ bag = Number(bag);
+ if(bag == num){
+     console.log("Yes");
+ }else{
+     console.log("No");
+ }
